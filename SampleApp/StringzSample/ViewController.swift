54ce4ccca632a4eac7f1e3d242b99dece8168636
Haytham KATBY <mohakapt@gmail.com>
2020-12-23 19:42:09 +0300

    ✨ Add sample app to use for test

---
+++ SampleApp/StringzSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StringzSample
+//
+//  Created by Heysem Katibi on 18.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
